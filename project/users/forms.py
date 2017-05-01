8f0670db52215327e5c7edf25f853a511d4477b4
Mridul Nagpal <mridulnagpal07@gmail.com>
2017-05-01 07:30:20 +0530

    sa

--- project/users/forms.py
+++ project/users/forms.py
@@ -30,16 +30,7 @@ class LoginForm(forms.Form):
     password = forms.CharField(label='', widget=forms.PasswordInput(
 	    	attrs={'placeholder': 'Password'}
 	    	))
-
-    def login(self,request):
-        if request.method == 'POST':
-            if self.is_valid():
-                user = authenticate(email=self.cleaned_data.get('email'), password=self.cleaned_data.get('password'))
-                if user:
-                    return user
-            raise forms.ValidationError("Sorry, that login was invalid. Please try again.")
-            return user
-            
+                    
 class SellerForm(forms.Form):
     class Meta(ModelForm):
         model = Item
